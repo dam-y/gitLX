b76f7b14225152abd06cec73e38420782c8de60c
dam-y <2750882656@qq.com>
2024-10-30 22:43:23 +0800

    第二次

--- ego-api-gateway/src/main/java/com/ego/gateway/EgoGateway.java
+++ ego-api-gateway/src/main/java/com/ego/gateway/EgoGateway.java
@@ -12,6 +12,8 @@ public class EgoGateway {
     public static void main(String[] args) {
         SpringApplication.run(EgoGateway.class);
         // 第一次提交
-        System.out.println("hello");
+        System.out.println("hello1");
+        // 第二次提交
+        System.out.println("hello2");
     }
 }
